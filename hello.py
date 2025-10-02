75cde790777d6c03bce93c276028da4c1c96e412
InwSaKodBeiw007 <gamekung2707@gmail.com>
2025-10-02 23:54:56 +0700

    create flask

---
+++ hello.py
@@ -0,0 +1,32 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     return "<h1>Hello world</h1>"
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello/')
+def hello():
+    return 'Hello, World'
+
+@app.route('/login')
+def login():
+    return 'login pages'
+
+@app.route('/users/<username>')
+def w_user(username):
+    return f'{username}\'s sites'
+
+from flask import url_for
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('hello'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('w_user', username='John Doe'))
