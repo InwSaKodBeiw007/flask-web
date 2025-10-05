5004564f251a96fc7b26a834a70fa48cecdb9994
InwSaKodBeiw007 <gamekung2707@gmail.com>
2025-10-05 22:20:27 +0700

    not work help me

--- uploads.py
+++ uploads.py
@@ -1,9 +1,12 @@
 from flask import Flask,render_template,send_from_directory,request,jsonify
-import os
+import os,requests
+from datetime import datetime
 
 app = Flask(__name__) 
 
 uploadPath = "uploads"
+# n8n Webhook URL
+N8N_WEBHOOK_URL = "http://localhost:5678/webhook/new-image"
 
 @app.route('/')
 def index():
@@ -22,7 +25,7 @@ def upanddown(filename):
 
 @app.route('/uploads',methods=["POST"])
 def uploadgimmic():
-## เช็ค 'file' ได้จาก request.files ได้เพราะเราต้องเข้าถึง url= http://localhost:8000/uploads ก่อน request ถึงจะมี requset.files ได้, ซึ่ง มันรอจนกว่าจะมีคนเข้าถึง def(function) นี้ถึงทำงาน
+## เช็ค 'file' ได้จาก request.files ได้เพราะเราต้องเข้าถึง url= http://host.docker.internal:8000/uploads ก่อน request ถึงจะมี requset.files ได้, ซึ่ง มันรอจนกว่าจะมีคนเข้าถึง def(function) นี้ถึงทำงาน
     if 'file' not in request.files:
         return jsonify({'error':"NO detect file"}),400
     
@@ -31,11 +34,35 @@ def uploadgimmic():
     filePath = os.path.join("uploads",file.filename)
     file.save(filePath)
 
+    # สร้าง URL สำหรับดาวน์โหลด
+    get_url_path = f"http://host.docker.internal:8000/uploads/{file.filename}"
+
+    ## แจ้ง n8n ว่ามีไฟล์ใหม่ (ส่ง metadata ไป)
+    try:
+
+        file_info = {
+            "filename": file.filename,
+            "url": get_url_path,    ## โยนให้เป็น get _ http://host.docker.internal:8000/
+            "timestamp": datetime.now().isoformat(),
+            "size": os.path.getsize(filePath),
+            "path": filePath
+        }
+
+        notify_response = requests.post(
+            N8N_WEBHOOK_URL,
+            json=file_info,
+            timeout=5
+        )
+        print(notify_response.status_code)
+        
+    except requests.exceptions.ConnectionError:
+        print("n8n no connection")
+
     return jsonify({
         "message":"Good point you uploaded files",
         "filename": file.filename,
-        "url":f"/uploads/{file.filename}"
+        "url":get_url_path   ## โยนให้เป็น get
     }),201
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(port=8000, debug=False)
